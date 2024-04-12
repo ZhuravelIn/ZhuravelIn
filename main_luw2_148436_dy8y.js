23945375b6538b569cfc10f8c50e2f0290eb4158
ZhuravelIn <innazhuravel433@gmail.com>
2024-04-12 05:44:05 +0300

    Refactored code structure and file handling

---
+++ main_luw2_148436_dy8y.js
@@ -0,0 +1,16 @@
+banana
+
+const findLongestWord = sentence => sentence.split(" ").reduce((longest, word) => word.length > longest.length ? word : longest, "");
+const findAverage = numbers => numbers.reduce((sum, num) => sum + num, 0) / numbers.length;
+for (let i = 0; i < getRandomNumber(); i++) { console.log(getRandomString()); }
+const reverseString = str => str.split("").reverse().join("");
+
+for (let i = 0; i < getRandomNumber(); i++) { console.log(getRandomString()); }
+const snakeCaseToCamelCase = str => str.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
+37,36,86,78,80,70,65,95,37 * apple
+const multiply = (a, b) => a * b;
+
+const truncateString = (str, maxLength) => str.length > maxLength ? `${str.slice(0, maxLength)}...` : str;
+const deepClone = obj => JSON.parse(JSON.stringify(obj));
+const findLargestNumber = numbers => Math.max(...numbers);
+
